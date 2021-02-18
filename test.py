5158659362a808ee6ca320055fb1f0f0b29bb6d5
atgane <hyper2012@naver.com>
2021-02-18 14:49:10 +0900

    update bilinear interpolate function

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import numpy as np
+import cv2
 
 a = cv2.imread(("OCR1.png"), cv2.IMREAD_GRAYSCALE)
 print(a.shape)
 
-z = np.zeros((100, 100))
+z = np.zeros((100, 100, 2))
 
 p1 = (10, 10)
 p2 = (15, 90)
@@ -18,6 +19,15 @@ def lll(_a, _z, _p1, _p2, _p3, _p4):
             x = round((1 - u) * ((1 - v) * _p1[0] + v * _p2[0]) + u * ((1 - v) * _p3[0] + v * _p4[0]))
             y = round((1 - u) * ((1 - v) * _p1[1] + v * _p2[1]) + u * ((1 - v) * _p3[1] + v * _p4[1]))
             try:
-                _z[x][y] = _a[round(_a.shape[0] * u)][round(_a.shape[1] * v)]
+                _z[x][y][0] = (_z[x][y][0] * _z[x][y][1] + _a[round(_a.shape[0] * u)][round(_a.shape[1] * v)]) / (_z[x][y][1] + 1)
+                _z[x][y][1] += 1
             except:
                 pass
+
+
+print(a[29][79])
+lll(a, z, p1, p2, p3, p4)
+print(z[:, :, 0])
+
+# _, z = cv2.threshold(z, 110, 255, cv2.THRESH_BINARY)
+cv2.imwrite("asd.png", z[:, :, 0])
