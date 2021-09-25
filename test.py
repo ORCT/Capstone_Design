96876abee996f851287a609848b9bc70a413089c
atgane <hyper2012@naver.com>
2021-09-25 10:27:04 +0900

    develop optimization
    코드 최적화 업데이트

--- test.py
+++ test.py
@@ -18,39 +18,144 @@ def load_image(_img, _state='w'):
 def conv_img2ser(_img):
     Y, X = _img.shape
     y, x = 0, 0
+    _state = 0
     ans1 = []
     
-    init_loc = [0, 0]
-    
-    if _img[0, 0] == 255:
-        ans1.append('p')
-        ans1.append('`')
-        ans1.append('P')
-        ans1.append('`')
-    
-    for i in range(Y - 1):
-        for j in range(X - 1):
-            if y % 2 == 0:
-                ans1.append('r')
-                ans1.append('1')
-                ans1.append('`')
-                x += 1
-            if y % 2 == 1:
-                ans1.append('l')
-                ans1.append('1')
-                ans1.append('`')
-                x -= 1
-            if _img[y, x] == 255:
+    while _state != 3:
+        if _state == 0:
+            if 255 in _img[y]:
+                _state = 2
+            else:
+                _state = 1
+                
+        elif _state == 1:
+            _down_flag = 0
+            for i in range(y + 1, Y):
+                if 255 in _img[i]:
+                    _down_flag = 1
+                    ans1.append('d')
+                    ans1.append(str(i - y))
+                    ans1.append('`')
+                    y = i
+                    break
+            if _down_flag == 0:
+                _state = 3
+            elif _down_flag == 1:
+                _state = 2
+                
+        elif _state == 2:
+            _r_max = 0
+            _l_max = 0
+            for i in range(X):
+                if _img[y, i] == 255:
+                    _l_max = i
+                    break
+            for i in range(X):
+                if _img[y, X - 1 - i] == 255:
+                    _r_max = X - 1 - i
+                    break
+            
+            if _r_max == _l_max:
                 ans1.append('p')
                 ans1.append('`')
                 ans1.append('P')
                 ans1.append('`')
+            
+            elif _r_max - x > x - _l_max:
+                if x - _l_max != 0:
+                    ans1.append('l')
+                    ans1.append(str(x - _l_max))
+                    ans1.append('`')
+                    x = _l_max
                 
-        ans1.append('d')
-        ans1.append('1')
-        ans1.append('`')
-        y += 1
-        
+                _push_flag = 0
+                
+                while x <= _r_max:
+                    if _img[y, x] == 255:
+                        if _push_flag == 0:
+                            ans1.append('p')
+                            ans1.append('`')
+                            ans1.append('P')
+                            ans1.append('`')
+                            ans1.append('r')
+                            ans1.append('1')
+                            ans1.append('`')
+                            _push_flag = 1
+                        elif _push_flag == 1:
+                            ans1.pop()
+                            ans1.pop()
+                            ans1.pop()
+                            ans1.pop()
+                            ans1.pop()
+                            ans1.append('r')
+                            ans1.append('1')
+                            ans1.append('`')
+                            ans1.append('P')
+                            ans1.append('`')
+                            ans1.append('r')
+                            ans1.append('1')
+                            ans1.append('`')
+                    else:
+                        _push_flag = 0
+                        ans1.append('r')
+                        ans1.append('1')
+                        ans1.append('`')
+                    if x < _r_max:
+                        x += 1
+                    else:
+                        ans1.pop()
+                        ans1.pop()
+                        ans1.pop()
+                        break
+                
+            else:
+                if _r_max - x != 0:
+                    ans1.append('r')
+                    ans1.append(str(_r_max - x))
+                    ans1.append('`')
+                    x = _r_max
+                
+                _push_flag = 0
+                
+                while x >= _l_max:
+                    if _img[y, x] == 255:
+                        if _push_flag == 0:
+                            ans1.append('p')
+                            ans1.append('`')
+                            ans1.append('P')
+                            ans1.append('`')
+                            ans1.append('l')
+                            ans1.append('1')
+                            ans1.append('`')
+                            _push_flag = 1
+                        elif _push_flag == 1:
+                            ans1.pop()
+                            ans1.pop()
+                            ans1.pop()
+                            ans1.pop()
+                            ans1.pop()
+                            ans1.append('l')
+                            ans1.append('1')
+                            ans1.append('`')
+                            ans1.append('P')
+                            ans1.append('`')
+                            ans1.append('l')
+                            ans1.append('1')
+                            ans1.append('`')
+                    else:
+                        _push_flag = 0
+                        ans1.append('l')
+                        ans1.append('1')
+                        ans1.append('`')
+                    if x > _l_max:
+                        x -= 1
+                    else:
+                        ans1.pop()
+                        ans1.pop()
+                        ans1.pop()
+                        break
+                    
+            _state = 1
         
     ans1 = deque(ans1)
     ans2 = []
@@ -103,7 +208,10 @@ def conv_ser2img(_ser, _img_shape):
     
     return ans
         
-a = load_image("output8.png", 'w')
+        
+### main        
+
+a = load_image("input9.png", 'w')
 
 print(a.shape)
 ser = conv_img2ser(a)
